5c9827fe521f0d125aef8d0797daa22cb4051e54
sylvainguelle <sguelle@gmail.com>
2019-07-14 16:54:05 +0200

    mise en place du formulaire

--- view/frontend/loginview.php
+++ view/frontend/loginview.php
@@ -1,19 +1,45 @@
 <?php $title = "Accueil"; ?>
 
 <?php ob_start(); ?>
-<div id="blog-login" class="row justify-content-between">
-  <div id="login" class="formulaire col-lg-12">
-    <form action="index.html" method="post">
-      <input type="text" name="" value="">
-      <input type="text" name="" value="">
-      <input type="submit" name="valider" value="valider">
+<div id="blog-login" class="row justify-content-center">
+  <div id="login" class="formulaire col-lg-5">
+    <h3>Formulaire d'incription</h3>
+    <form class="" action="cible.php" method="post">
+      <div class="">
+        <label for="firstname">Nom : </label>
+        <input type="text" name="firstname" required>
+      </div>
+      <div class="">
+        <label for="lastname">Prénom : </label>
+        <input type="text" name="lastname" required>
+      </div>
+      <div class="">
+        <label for="email">Adresse mail : </label>
+        <input type="email" name="email" value="" required>
+      </div>
+      <div class="">
+        <label for="password">Mot de passe : </label>
+        <input type="password" name="password" value="" required>
+      </div>
+      <div class="">
+        <input type="submit" name="envoyer" required>
+      </div>
     </form>
   </div>
-  <div id="inscription" class="formulaire col-lg-12">
-    <form action="index.html" method="post">
-      <input type="text" name="" value="">
-      <input type="text" name="" value="">
-      <input type="submit" name="valider" value="valider">
+  <div id="inscription" class="formulaire col-lg-5">
+    <h3>Déja inscrit ? Connectez vous ici!</h3>
+    <form class="" action="cible.php" method="post">
+      <div class="">
+        <label for="email">Adresse mail : </label>
+        <input type="email" name="email" value="" required>
+      </div>
+      <div class="">
+        <label for="password">Mot de passe : </label>
+        <input type="password" name="password" value="" required>
+      </div>
+      <div class="">
+        <input type="submit" name="envoyer" required>
+      </div>
     </form>
   </div>
 </div>
